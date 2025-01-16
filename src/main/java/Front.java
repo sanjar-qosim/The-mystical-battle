3f23d4c9adb1d9c783541e73e263ee59448f0f51
sanjar-qosim <sanjar171766@gmail.com>
2025-01-17 00:11:05 +0500

    Added methods: create hero and show info

--- src/main/java/Front.java
+++ src/main/java/Front.java
@@ -1,6 +1,15 @@
 public class Front {
     public static void main(String[] args) {
+
         Mechanic mechanic = new Mechanic();
-        System.out.println(mechanic.askHero());
+
+        String ask = mechanic.askHero();
+        GeneralAttributes hero = mechanic.createHero(ask);
+
+        String ask1 = mechanic.askHero();
+        GeneralAttributes hero1 = mechanic.createHero(ask1);
+
+        mechanic.showInfo(hero);
+        mechanic.showInfo(hero1);
     }
 }
--- src/main/java/Mechanic.java
+++ src/main/java/Mechanic.java
@@ -4,19 +4,78 @@ public class Mechanic {
 
     Scanner scanner = new Scanner(System.in);
 
-    public String askHero(){
-        System.out.println("Which type hero do you want to create?\n-Mage;\nShaman;\nPalladin?");
-        while (true) {
-            String askedHero = scanner.nextLine();
-            if (askedHero.equals("Mage") || askedHero.equals("Shaman") || askedHero.equals("Palaldin")) {
-                return askedHero;
-            } else if (askedHero.equals("exit")) {
-                return null;
-            } else {
-                System.out.println("There is not a type what you entered!");
-                return null;
-            }
+    String askedHero;
 
+    public String askHero(){
+        System.out.println("Which type hero do you want to create?\nMage;\nShaman;\nPaladin?");
+        while (true) {
+            String askHero = scanner.nextLine();
+            if(askHero.equals("Mage") || askHero.equals("Shaman") || askHero.equals("Paladin")) {
+                askedHero = askHero;
+                return askedHero;
+            } else if (askHero.equals("exit")) {
+                break;
+            } else {
+                System.out.println("You enter wrong type! Please, enter right type or exit program!");
+            }
+        }
+        return null;
+    }
+
+    public GeneralAttributes createHero(String askedHero){
+        if (askedHero.equals("Mage")) {
+            System.out.println("Enter a name of your hero:");
+            String name = scanner.nextLine();
+            System.out.println("Enter a health of your hero:");
+            double health = scanner.nextDouble();
+            System.out.println("Enter a damage of your hero:");
+            double damage = scanner.nextDouble();
+            return Mage.builder()
+                    .name(name)
+                    .health(health)
+                    .damage(damage)
+                    .build();
+        }
+
+        if (askedHero.equals("Shaman")) {
+            System.out.println("Enter a name of your hero:");
+            String name = scanner.nextLine();
+            System.out.println("Enter a health of your hero:");
+            double health = scanner.nextDouble();
+            return Shaman.builder()
+                    .name(name)
+                    .health(health)
+                    .build();
+        }
+
+        if (askedHero.equals("Paladin")) {
+            System.out.println("Enter a name of your hero:");
+            String name = scanner.nextLine();
+            System.out.println("Enter a health of your hero:");
+            double health = scanner.nextDouble();
+            System.out.println("Enter a damage of your hero:");
+            double damage = scanner.nextDouble();
+            return Paladin.builder()
+                    .name(name)
+                    .health(health)
+                    .damage(damage)
+                    .build();
+        }
+
+        return null;
+    }
+
+    public void showInfo(GeneralAttributes hero){
+        if (hero.getClass() == Mage.class) {
+            ((Mage) hero).showInfo();
+        }
+
+        if (hero.getClass() == Shaman.class) {
+            ((Shaman) hero).showInfo();
+        }
+
+        if (hero.getClass() == Paladin.class) {
+            ((Paladin) hero).showInfo();
         }
     }
 }
